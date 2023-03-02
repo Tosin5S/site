ad5c7b47ca5bddffbef86f61224af669e74ec5f3
Tosin5S <olyguy@gmail.com>
2023-03-02 20:49:18 +0100

    add tailwindcss configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-white h-screen flex justify-center items-center">
+      <div className="bg-gray-100 rounded-lg p-8">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+        <p className="text-xl mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="flex justify-center items-center space-x-4">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-lg transition duration-300">
+            Login
+          </button>
+          <button className="bg-green-500 hover:bg-green-600 text-white py-3 px-6 rounded-lg transition duration-300">
+            Sign Up
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
